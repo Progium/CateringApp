6b622e0762dc7502d4a99a2249694a44f57f57cf
Yuli <yuli0192@gmail.com>
2015-07-05 17:54:43 -0600

    arreglo ejb

--- src/main/webapp/resources/modulos/seguridad/usuario-registrar-controller.js
+++ src/main/webapp/resources/modulos/seguridad/usuario-registrar-controller.js
@@ -6,19 +6,7 @@
  */
 
 App.controller('UsuarioRegistrarController', function($scope, $location) {
-	$scope.onlyNumbers = "^[0-9]*\.?[0-9]+$";
-	
-	$scope.phoneNumberPattern = (function() {
-	    var regexp = /^\d{0,9}(\.\d{1,9})?$/;
-	    return {
-	        test: function(value) {
-	            if( $scope.requireTel === false ) {
-	                return true;
-	            }
-	            return regexp.test(value);
-	        }
-	    };
-	})();
+
 	$scope.cancelar = function () {
 		$location.path('/iniciar-sesion');
 	}
