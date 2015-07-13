c9dc81ea80826d79efe29757eaa76e0e31ea29b2
josue <josueperez.sol@gmail.com>
2015-07-12 21:02:39 -0600

    Avance de funcionalidad y corrección de WSFilter

---
+++ src/main/java/com/progium/catering/controllers/ProductoController.java
@@ -0,0 +1,63 @@
+package com.progium.catering.controllers;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.progium.catering.ejb.Producto;
+import com.progium.catering.contracts.BaseResponse;
+import com.progium.catering.contracts.ProductoResponse;
+import com.progium.catering.services.ProductoServiceInterface;
+import com.progium.catering.pojo.ProductoPOJO;
+import com.progium.catering.utils.PojoUtils;
+
+
+@RestController
+@RequestMapping(value = "rest/protected/producto")
+public class ProductoController {
+	
+	@Autowired
+	ProductoServiceInterface productoService;
+	
+	@Autowired
+	HttpServletRequest request;	
+	
+	@RequestMapping(value = "/getAll", method = RequestMethod.GET)
+	@Transactional
+	public ProductoResponse getAll(){	
+		
+		ProductoResponse entityResponse = new ProductoResponse();
+		List<Producto> productos = productoService.findAll();
+		
+		if(productos != null){
+			entityResponse.setCode(200);
+			entityResponse.setCodeMessage("productos fetch success");
+			List<ProductoPOJO> productosPOJO = new ArrayList<ProductoPOJO>();
+			
+			for(Producto producto : productos){
+				ProductoPOJO productoPojo = new ProductoPOJO();
+				PojoUtils.pojoMappingUtility(productoPojo, producto);
+				productosPOJO.add(productoPojo);
+			}
+			entityResponse.setProductos(productosPOJO);
+		}else{
+			entityResponse.setCode(400);
+			entityResponse.setErrorMessage("productos could not be retrieved");
+		}
+		return entityResponse;
+	}	
+	
+}
---
+++ src/main/java/com/progium/catering/services/ProductoServiceInterface.java
@@ -0,0 +1,10 @@
+package com.progium.catering.services;
+
+import java.util.List;
+import com.progium.catering.ejb.Producto;
+
+public interface ProductoServiceInterface {
+
+	List<Producto> findAll();
+	
+}
